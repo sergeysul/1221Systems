c237e2b5308e3f979b08fa8e716da19273d4f5bd
elc <hm8huh@mail.ru>
2025-06-08 20:54:57 +0300

    Initial Commit

---
+++ Weather1221/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather1221
+//
+//  Created by Сергей Сулятецкий on 08.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
